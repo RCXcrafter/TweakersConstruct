06c32cffa6fb8e5cdff679c8f3aacc9fb0f5e867
RCXcrafter <rcxcrafter@gmail.com>
2019-01-31 12:43:29 +0100

    fix issue with modifying sharpening kit cost

--- src/main/java/com/rcx/tweaconstruct/tweaks/PatternCosts.java
+++ src/main/java/com/rcx/tweaconstruct/tweaks/PatternCosts.java
@@ -7,6 +7,7 @@ import com.rcx.tweaconstruct.ConfigHandler;
 import net.minecraft.item.Item;
 import net.minecraft.util.ResourceLocation;
 import slimeknights.tconstruct.library.materials.Material;
+import slimeknights.tconstruct.library.tools.ToolPart;
 
 public class PatternCosts {
 	public static void init() {
@@ -18,21 +19,25 @@ public class PatternCosts {
 			if (entries.length != 3)
 				continue;
 			Item partItem = Item.REGISTRY.getObject(new ResourceLocation(entries[0], entries[1]));
-			if (partItem == null)
+			if (partItem == null || !(partItem instanceof ToolPart))
 				continue;
 			try {
-				Field f = partItem.getClass().getDeclaredField("cost");
+				Field f = ToolPart.class.getDeclaredField("cost");
 				f.setAccessible(true);
 				try {
 					f.setInt(partItem, (int) (Material.VALUE_Ingot * Double.parseDouble(entries[2])));
 				} catch (IllegalArgumentException e) {
+					e.printStackTrace();
 					continue;
 				} catch (IllegalAccessException e) {
+					e.printStackTrace();
 					continue;
 				}
 			} catch (NoSuchFieldException e) {
+				e.printStackTrace();
 				continue;
 			} catch (SecurityException e) {
+				e.printStackTrace();
 				continue;
 			}
 		}
